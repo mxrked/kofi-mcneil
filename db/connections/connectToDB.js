ee2bdbaaaf02cdb454e1850ce0475ed0693fc4c4
Parker <pp101socials@gmail.com>
2023-11-05 07:58:16 -0500

    Changed URI

--- db/connections/connectToDB.js
+++ db/connections/connectToDB.js
@@ -12,8 +12,6 @@ const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-const URI =
-  "mongodb+srv://admin:softskillitcommunications_DB_020700@ip-storage.tjqlwwg.mongodb.net/?retryWrites=true&w=majority";
 
 let client;
 let connection;
@@ -21,7 +19,7 @@ let connection;
 export async function connectDatabase() {
   try {
     if (!client || !client.connect()) {
-      client = new MongoClient(URI, OPTIONS);
+      client = new MongoClient(process.env.MONGO_URI, OPTIONS);
 
       if (!(await client.connect())) {
         console.log("Connecting to MongoDB failed");
