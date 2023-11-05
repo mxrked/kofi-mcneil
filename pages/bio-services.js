f1f3ba1304407eaf0d066bce9de20add786249a7
Parker <pp101socials@gmail.com>
2023-11-05 07:22:19 -0500

    Added content

--- pages/bio-services.js
+++ pages/bio-services.js
@@ -1,22 +1,129 @@
 // React/Next Imports
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import fs from "fs";
+import path from "path";
 
 // Library Imports
 
 // Data/Functions/Images Imports
+import { connectDatabase } from "@/db/connections/connectToDB";
+import checkLocation from "@/assets/functions/async/checkLocation";
 
 // Component Imports
 import { LoadingScreen } from "@/assets/components/global/All/LoadingScreen";
+import { PageHead } from "@/assets/components/global/All/PageHead";
 
 // Style Imports
 // import "../assets/styles/modules/BioServices/BioServices.module.css";
 
-export default function BioServices() {
+export async function getServerSideProps() {
+  // Connecting to DB
+  try {
+    const DB = await connectDatabase();
+
+    if (!DB) {
+      return {
+        props: {
+          TOTAL_NUMBER_OF_IPS: 0,
+          PH_BIOSERVICES: null,
+          PH_ICONS: null,
+        },
+      };
+    }
+
+    // Checking if DB was connected
+    if (DB) {
+      console.log("Connected to DB!");
+    }
+
+    const TOTAL_NUMBER_OF_IPS = await DB.collection("ips").countDocuments();
+
+    const PH_ICONS_FILE_PATH = path.join(
+      process.cwd(),
+      "public/data/json/page-head-data/",
+      "PH_Icons.json"
+    );
+    const PH_BIO_SERVICES_FILE_PATH = path.join(
+      process.cwd(),
+      "public/data/json/page-head-data/",
+      "PH_BioServices.json"
+    );
+
+    const PH_ICONS_FILE_CONTENTS = fs.readFileSync(PH_ICONS_FILE_PATH, "utf-8");
+    const PH_BIO_SERVICES_FILE_CONTENTS = fs.readFileSync(
+      PH_BIO_SERVICES_FILE_PATH,
+      "utf-8"
+    );
+
+    const PH_ICONS = JSON.parse(PH_ICONS_FILE_CONTENTS);
+    const PH_BIOSERVICES = JSON.parse(PH_BIO_SERVICES_FILE_CONTENTS);
+
+    return {
+      props: {
+        TOTAL_NUMBER_OF_IPS,
+        PH_BIOSERVICES,
+        PH_ICONS,
+      },
+    };
+  } catch (error) {
+    console.error("Error while fetching data:", error);
+    return {
+      props: {
+        TOTAL_NUMBER_OF_IPS: 0,
+        PH_BIOSERVICES: null,
+        PH_ICONS: null,
+      },
+    };
+  }
+}
+
+export default function BioServices({
+  TOTAL_NUMBER_OF_IPS,
+  PH_ICONS,
+  PH_BIOSERVICES,
+}) {
   const router = useRouter();
+
+  // Triggering trackWebsiteVisits.js
+  useEffect(() => {
+    const FETCH_WEBSITE_VISITS = async () => {
+      try {
+        const isOnLocalHost = await checkLocation();
+        const response = await fetch("/api/trackWebsiteVisits.js");
+
+        if (response.ok) {
+          const data = await response.json();
+
+          if (isOnLocalHost) {
+            console.log("-- On Localhost --");
+            console.log("Fetched Data: ", data);
+          }
+        } else {
+          console.log("-- On Localhost --");
+          console.error("Fetch request failed with status: ", response.status);
+          return;
+        }
+      } catch (error) {
+        console.error("Error while fetching data: ", error);
+      }
+    };
+
+    FETCH_WEBSITE_VISITS();
+  }, []);
+
+  // Displaying the number of page visits
+  useEffect(() => {
+    if (TOTAL_NUMBER_OF_IPS > 0) {
+      console.log("Website Visits: " + TOTAL_NUMBER_OF_IPS);
+    } else {
+      console.log("There are no website visits..");
+    }
+  }, []);
 
   return (
     <div id="PAGE" className="page half-second">
+      <PageHead page_head_data={PH_BIOSERVICES} icons_data={PH_ICONS} />
       <LoadingScreen />
 
       <div id="PAGE_CNT" className="page-cnt half-second">
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,122 @@
 // React/Next Imports
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import fs from "fs";
+import path from "path";
 
 // Library Imports
 
 // Data/Functions/Images Imports
+import { connectDatabase } from "@/db/connections/connectToDB";
+import checkLocation from "@/assets/functions/async/checkLocation";
 
 // Component Imports
 import { LoadingScreen } from "@/assets/components/global/All/LoadingScreen";
+import { PageHead } from "@/assets/components/global/All/PageHead";
 
 // Style Imports
 import "../assets/styles/modules/Index/Index.module.css";
 
-export default function Home() {
+export async function getServerSideProps() {
+  // Connecting to DB
+  try {
+    const DB = await connectDatabase();
+
+    if (!DB) {
+      return {
+        props: {
+          TOTAL_NUMBER_OF_IPS: 0,
+          PH_INDEX: null,
+          PH_ICONS: null,
+        },
+      };
+    }
+
+    // Checking if DB was connected
+    if (DB) {
+      console.log("Connected to DB!");
+    }
+
+    const TOTAL_NUMBER_OF_IPS = await DB.collection("ips").countDocuments();
+
+    const PH_ICONS_FILE_PATH = path.join(
+      process.cwd(),
+      "public/data/json/page-head-data/",
+      "PH_Icons.json"
+    );
+    const PH_INDEX_FILE_PATH = path.join(
+      process.cwd(),
+      "public/data/json/page-head-data/",
+      "PH_Index.json"
+    );
+
+    const PH_ICONS_FILE_CONTENTS = fs.readFileSync(PH_ICONS_FILE_PATH, "utf-8");
+    const PH_INDEX_FILE_CONTENTS = fs.readFileSync(PH_INDEX_FILE_PATH, "utf-8");
+
+    const PH_ICONS = JSON.parse(PH_ICONS_FILE_CONTENTS);
+    const PH_INDEX = JSON.parse(PH_INDEX_FILE_CONTENTS);
+
+    return {
+      props: {
+        TOTAL_NUMBER_OF_IPS,
+        PH_INDEX,
+        PH_ICONS,
+      },
+    };
+  } catch (error) {
+    console.error("Error while fetching data:", error);
+    return {
+      props: {
+        TOTAL_NUMBER_OF_IPS: 0,
+        PH_INDEX: null,
+        PH_ICONS: null,
+      },
+    };
+  }
+}
+
+export default function Home({ TOTAL_NUMBER_OF_IPS, PH_ICONS, PH_INDEX }) {
   const router = useRouter();
+
+  // Triggering trackWebsiteVisits.js
+  useEffect(() => {
+    const FETCH_WEBSITE_VISITS = async () => {
+      try {
+        const isOnLocalHost = await checkLocation();
+        const response = await fetch("/api/trackWebsiteVisits.js");
+
+        if (response.ok) {
+          const data = await response.json();
+
+          if (isOnLocalHost) {
+            console.log("-- On Localhost --");
+            console.log("Fetched Data: ", data);
+          }
+        } else {
+          console.log("-- On Localhost --");
+          console.error("Fetch request failed with status: ", response.status);
+          return;
+        }
+      } catch (error) {
+        console.error("Error while fetching data: ", error);
+      }
+    };
+
+    FETCH_WEBSITE_VISITS();
+  }, []);
+
+  // Displaying the number of page visits
+  useEffect(() => {
+    if (TOTAL_NUMBER_OF_IPS > 0) {
+      console.log("Website Visits: " + TOTAL_NUMBER_OF_IPS);
+    } else {
+      console.log("There are no website visits..");
+    }
+  }, []);
 
   return (
     <div id="PAGE" className="page half-second">
+      <PageHead page_head_data={PH_INDEX} icons_data={PH_ICONS} />
       <LoadingScreen />
 
       <div id="PAGE_CNT" className="page-cnt half-second">
