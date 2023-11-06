e964a72a163a3502f09968df4b16427519a68035
Parker <pp101socials@gmail.com>
2023-11-06 06:38:32 -0500

    Added CDN

--- assets/cdns/CDNImgs.js
+++ assets/cdns/CDNImgs.js
@@ -6,4 +6,27 @@
 
 import { CDNImgReturn } from "./CDNReturns";
 
-export {};
+const CISO_OFFICER_IMG = CDNImgReturn("bio-services", "ciso-officer", "webp");
+const PENETRATION_TESTER_IMG = CDNImgReturn(
+  "bio-services",
+  "penetration-tester",
+  "webp"
+);
+const PROGRAMMER_DEVELOPER_IMG = CDNImgReturn(
+  "bio-services",
+  "programmer-developer",
+  "webp"
+);
+const RED_BLUE_TEAM_IMG = CDNImgReturn("bio-services", "red-blue-team", "webp");
+const SOC_ANALYST_IMG = CDNImgReturn("bio-services", "soc-analyst", "webp");
+
+const CONTACT_FORM_IMG = CDNImgReturn("contact", "contact-form-img", "webp");
+
+export {
+  CISO_OFFICER_IMG,
+  PENETRATION_TESTER_IMG,
+  PROGRAMMER_DEVELOPER_IMG,
+  RED_BLUE_TEAM_IMG,
+  SOC_ANALYST_IMG,
+};
+export { CONTACT_FORM_IMG };
