19ad4846ce65553ddd8ac63cb2a05761f098a756
Parker <pp101socials@gmail.com>
2023-11-06 07:00:06 -0500

    Created file

---
+++ assets/components/global/All/page-heads/PH_Contact.js
@@ -0,0 +1,229 @@
+/**
+ *
+ *  This is the Page Head
+ *
+ */
+
+import { useRouter } from "next/router";
+
+import Head from "next/head";
+
+export const PH_Contact = () => {
+  const router = useRouter();
+
+  const KEYWORDS = [
+    "Contact Kofi Mcneil",
+    "Book a Consultation",
+    "IT Public Speaking Expert",
+    "IT Communication Consultation",
+    "Dynamic IT Communicator",
+    "Consultation Meeting with Kofi Mcneil",
+    "Calendly Appointment Booking",
+    "Contact Form for Consultation",
+    "Consultation Booking Issues",
+    "Unlock Your Communication Potential",
+    "Reach Out to Kofi Mcneil",
+    "Schedule a Meeting with Kofi Mcneil",
+    "Contact Kofi Mcneil for Booking",
+    "IT Communication Appointment",
+    "Programmers Consultation",
+    "Developers Consultation",
+    "Penetration Testers Consultation",
+    "Red Team Consultation",
+    "Blue Team Consultation",
+    "SOC Analysts Consultation",
+    "CISO Officers Consultation",
+  ];
+
+  return (
+    <Head>
+      <title>
+        Softskillitcommunications - Contact Kofi Mcneil for IT Communication
+        Consultation
+      </title>
+
+      <meta name="keywords" content={KEYWORDS} />
+      <meta
+        name="description"
+        content={
+          "Contact us at Softskillitcommunications to book a consultation with Kofi Mcneil, your IT public speaking expert. Unlock your communication potential and enhance your IT skills. Whether you're a programmer, developer, penetration tester, SOC analyst, or CISO officer, we're here to assist you. Use our Calendly appointment booking or the contact form to schedule your consultation today."
+        }
+      />
+      <meta name="robots" content={"index, follow"} />
+      <link
+        rel="canonical"
+        href={"https://softskillitcommunications.com/contact/"}
+      />
+
+      {/** LINKEDIN */}
+      <meta
+        property="og:linkedin"
+        content={"https://www.linkedin.com/in/kofi-mcneil-319b13238/"}
+      />
+
+      <link
+        rel="icon"
+        type="image/x-icon"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon.ico"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-16x16.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-32x32.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="48x48"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-48x48.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="64x64"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-64x64.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-96x96.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="128x128"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-128x128.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-192x192.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="512x512"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/favicon/favicon-512x512.png"
+        }
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/apple-touch/apple-touch-icon-57x57.png"
+        }
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/apple-touch/apple-touch-icon-76x76.png"
+        }
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/apple-touch/apple-touch-icon-120x120.png"
+        }
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/apple-touch/apple-touch-icon-152x152.png"
+        }
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/apple-touch/apple-touch-icon-180x180.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="72x72"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/android/android-icon-72x72.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/android/android-icon-96x96.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="144x144"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/android/android-icon-144x144.png"
+        }
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/android/android-icon-192x192.png"
+        }
+      />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta
+        name="msapplication-TileImage"
+        content={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/ms-app/ms-app-icon-32x32.png"
+        }
+      />
+      <meta
+        name="msapplication-square70x70logo"
+        content={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/ms-app/ms-app-icon-70x70.png"
+        }
+      />
+      <meta
+        name="msapplication-square150x150logo"
+        content={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/ms-app/ms-app-icon-150x150.png"
+        }
+      />
+      <meta
+        name="msapplication-wide310x150logo"
+        content={
+          "https://raw.githubusercontent.com/mxrked/Freelance_projects_CDN/main/kofi-mcneil/icons/tab-icons/ms-app/ms-app-icon-310x150.png"
+        }
+      />
+    </Head>
+  );
+};
